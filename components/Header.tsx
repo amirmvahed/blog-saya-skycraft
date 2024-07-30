a9b742e39e29106d4262b7761f27e6abf2638bba
amirmvahed <amirmvahed@gmail.com>
2024-07-30 04:47:30 -0400

    Feat: Created Header component and added it to homepage

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import {assets} from '@/assets/assets'
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div className={'py-5 px-5 md:px-12 lg:px-28'}>
+            <div className={'flex justify-between items-center'}>
+                <Image src={assets.logo} width={180} alt={'logo'} className={'w-[130px] sm:w-auto'}/>
+                <button
+                    className={'flex items-center gap-2 font-medium text-xs py-1 px-1 sm:text-sm sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000]'}>
+                    Get started <Image src={assets.arrow} alt={'Get started'}/>
+                </button>
+            </div>
+            <div className={'text-center my-8'}>
+                <h1 className={'text-3xl sm:text-5xl font-medium'}>Latest Blogs</h1>
+                <p className={'my-10 max-w-[740px] m-auto text-xs sm:text-base'}>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Ad ipsa mollitia, perspiciatis quisquam
+                    unde veritatis.</p>
+                <form
+                    className={'flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto border border-black shadow-[-7px_7px_0px_#000]'}
+                    action="">
+                    <input type="email" placeholder={'Enter your email'} className={'pl-4 outline-none'}/>
+                    <button
+                        type={'submit'}
+                        className={'border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white'}>Subscribe
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Header
+
