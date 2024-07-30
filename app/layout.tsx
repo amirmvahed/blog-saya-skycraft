0c8e71030af2643ba36768ffecd0d1ea4ce46cfe
amirmvahed <amirmvahed@gmail.com>
2024-07-30 10:49:52 -0400

    fix: Deleted extra weights causing failure in loading fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from "next";
 import {Outfit} from "next/font/google";
 import "./globals.css";
 
-const outfit = Outfit({subsets: ["latin"], weight: ['400', '500', '600', '700']});
+const outfit = Outfit({subsets: ["latin"]});
 
 export const metadata: Metadata = {
     title: "Blog App",
