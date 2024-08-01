f83e3f1a2f9d2de20465a971237b7fe69b6526c8
amirmvahed <amirmvahed@gmail.com>
2024-08-01 06:52:56 -0400

    fix: I fixed a typo

--- components/BlogItem.tsx
+++ components/BlogItem.tsx
@@ -15,7 +15,7 @@ export default function BlogItem({id, title, description, category, image}: blog
         <Link href={`/blogs/${id}`}>
             <div
                 className={'max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000]'}>
-                <Image src={image} alt={title} width={400} heigth={400}
+                <Image src={image} alt={title} width={400} height={400}
                        className={'border-b border-black'}/>
                 <p className={'ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'}>{category}</p>
                 <div className={'p-5'}>
