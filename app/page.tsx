c2682fd3d48b9fd9001c8cb49cc3447a7a18af2e
amirmvahed <amirmvahed@gmail.com>
2024-08-01 09:40:35 -0400

    ref: Moving Footer to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import BlogList from "@/components/BlogList";
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 export default function Home() {
@@ -7,7 +6,6 @@ export default function Home() {
         <>
             <Header/>
             <BlogList/>
-            <Footer/>
         </>
     );
 }
