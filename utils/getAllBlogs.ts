be414007ef4ccbf17bbd1944817eb5d696ba524f
amirmvahed <amirmvahed@gmail.com>
2024-08-02 10:00:42 -0400

    test commit to find a bug

--- utils/getAllBlogs.ts
+++ utils/getAllBlogs.ts
@@ -1,14 +1,14 @@
-export const getAllBlogs = async () => {
-    try {
-        const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/posts/', {cache: 'no-cache'})
-
-        if (!response.ok) {
-            const errorData = await response.json()
-            console.error(errorData.message)
-        }
-
-        return response.json()
-    } catch (error) {
-        console.error("Error fetching blogs:", error)
-    }
-};
+// export const getAllBlogs = async () => {
+//     try {
+//         const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/posts/', {cache: 'no-cache'})
+//
+//         if (!response.ok) {
+//             const errorData = await response.json()
+//             console.error(errorData.message)
+//         }
+//
+//         return response.json()
+//     } catch (error) {
+//         console.error("Error fetching blogs:", error)
+//     }
+// };
